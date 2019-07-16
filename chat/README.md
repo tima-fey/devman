a1efa54775bb4b74e9f84e329d69e9b80b9317d0
Timofey <timonnius@gmail.com>
2019-07-17 00:02:29 +0300

    almast done

--- chat/README.md
+++ chat/README.md
@@ -23,14 +23,19 @@ by default server connect to 5000 and 5050 port
 ```Bash
 python3 chat.py --help
 usage: chat.py [-h] [--host HOST] [--rport RPORT] [--sport SPORT]
+               [--user USER] [--token_file TOKEN_FILE] [--text TEXT]
 
 connect to secret chat
 
 optional arguments:
-  -h, --help     show this help message and exit
-  --host HOST    Host to connect
-  --rport RPORT  Specify port to receive msg
-  --sport SPORT  Specify port to send msg
+  -h, --help            show this help message and exit
+  --host HOST           Host to connect
+  --rport RPORT         Specify port to receive msg
+  --sport SPORT         Specify port to send msg
+  --user USER           set a username, it is oblicated for first run
+  --token_file TOKEN_FILE
+                        set a file with token
+  --text TEXT           set a text to send
 ```
 
 # Project goals
--- chat/chat.py
+++ chat/chat.py
@@ -8,24 +8,23 @@ import json
 from aiofile import AIOFile
 
 
-async def get_text_from_socket(host, port, async_function, args):
+async def connector(host, port, async_function, args):
     timer = 0
     try:
-        reader, writer = await asyncio.open_connection(host=host,port=port)
+        reader, writer = await asyncio.open_connection(host=host, port=port)
     except (ConnectionRefusedError, ConnectionResetError):
         logging.warning('sleep %s seconds', 2 ** timer)
         await asyncio.sleep(2 ** timer)
-        reader, writer = await asyncio.open_connection(host=host,port=port)
-        timer += 1        
+        reader, writer = await asyncio.open_connection(host=host, port=port)
+        timer += 1
     while True:
         try:
             await async_function(reader, writer, args)
         except (ConnectionRefusedError, ConnectionResetError):
             logging.warning('sleep %s seconds', 2 ** timer)
             await asyncio.sleep(2 ** timer)
-            reader, writer = await asyncio.open_connection(host=host,port=port)
+            reader, writer = await asyncio.open_connection(host=host, port=port)
             timer += 1
-
 
 async def read_from_socket(reader, *_):
     text = await reader.readline()
@@ -35,7 +34,32 @@ async def read_from_socket(reader, *_):
     print(text.decode("utf-8"))
 
 
-async def send_text_to_socket(reader, writer, args):
+async def register(reader, writer, args):
+    if not args.user:
+        logging.error("It's obligated to specidy login if you do not have the correct token file")
+        logging.error('exiting')
+        sys.exit()
+    temp = await reader.readline()
+    logging.debug(temp.decode("utf-8"))
+    user = '{}\n'.format(args.user.replace('\n', ' '))
+    writer.write(user.encode())
+    answer = await reader.readline()
+    logging.debug(answer.decode("utf-8"))
+    answer_dict = json.loads(answer)
+    token = answer_dict['account_hash']
+    logging.debug(token)
+    async with AIOFile(args.token_file, 'w') as _file:
+        await _file.write(token)
+
+async def authorise(reader, writer, args, token):
+    writer.write('{}\n'.format(token.replace('\n', '')).encode())
+    answer = await reader.readline()
+    logging.debug(answer.decode("utf-8"))
+    if answer.decode("utf-8") == 'null\n':
+        logging.warning("Wrong token, let's get another one")
+        await register(reader, writer, args)
+
+async def submit_message(reader, writer, args):
     try:
         async with AIOFile(args.token_file, 'r') as _file:
             token = await _file.read()
@@ -44,41 +68,31 @@ async def send_text_to_socket(reader, writer, args):
     temp = await reader.readline()
     logging.debug(temp.decode("utf-8"))
     if not token:
-        if not args.user:
-            logging.error("To connect to the chat please specify the username. it's oblicated only for the first run")
-            logging.error('exiting')
-            sys.exit()
         writer.write('\n'.encode())
-        temp = await reader.readline()
-        logging.debug(temp.decode("utf-8"))
-        user = '{}\n'.format(args.user)
-        writer.write(user.encode())
-        answer = await reader.readline()
-        logging.debug(answer.decode("utf-8"))
-        answer_dict = json.loads(answer)
-        token = answer_dict['account_hash']
-        logging.debug(token)
-        async with AIOFile(args.token_file, 'w') as _file:
-            await _file.write(token)
+        await register(reader, writer, args)
     else:
-        writer.write('{}\n'.format(token).encode())
-    await asyncio.sleep(2)
-    writer.write('{}\n'.format(args.text).encode())
-        
+        await authorise(reader, writer, args, token)
+    writer.write('{}\n\n'.format(args.text.replace('\n', ' ')).encode())
+    raise StopIteration
+
 async def main():
+    # logging.basicConfig(level=logging.DEBUG)
     parser = argparse.ArgumentParser(description='connect to secret chat')
     parser.add_argument('--host', default='minechat.dvmn.org', help='Host to connect')
     parser.add_argument('--rport', default=5000, type=int, help='Specify port to receive msg')
     parser.add_argument('--sport', default=5050, type=int, help='Specify port to send msg')
     parser.add_argument('--user', help="set a username, it's oblicated for first run")
     parser.add_argument('--token_file', default="token.txt", help="set a file with token")
-    parser.add_argument('--text', default="test text", help="set a text to send after registration")
+    parser.add_argument('--text', default="test text", help="set a text to send")
 
     args = parser.parse_args()
-    task_1 = asyncio.create_task(get_text_from_socket(args.host, args.rport, read_from_socket, args))
-    task_2 = asyncio.create_task(get_text_from_socket(args.host, args.sport, send_text_to_socket, args))
+    task_1 = asyncio.create_task(connector(args.host, args.rport, read_from_socket, args))
+    task_2 = asyncio.create_task(connector(args.host, args.sport, submit_message, args))
     await task_1
     await task_2
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
